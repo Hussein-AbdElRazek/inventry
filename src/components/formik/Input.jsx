cae19a376272899453e68d677c1fc6c9b18bf1f0
Hussein <abdelrazeqhussein.371@gmail.com>
2023-09-27 18:27:59 +0300

    hide password in input

--- src/components/formik/Input.jsx
+++ src/components/formik/Input.jsx
@@ -19,24 +19,24 @@ function Input(props)
             {({ field, form }) =>
             {
                 return (
-                        <Tooltip title={form.values[name]}>
-                            <TextField
-                                name={name}
-                                id={name}
-                                type={type}
-                                label={label}
-                                disabled={disabled}
-                                error={form.errors[name] && form.touched[name] ?
-                                    true : false}
-                                helperText={form.errors[name] && form.touched[name] ?
-                                    form.errors[name] : " "}
-                                fullWidth
-                                variant="outlined"
-                                sx={{ mb: 1 }}
-                                {...field}
-                                {...rest}
-                            />
-                        </Tooltip>
+                    <Tooltip title={type !== "password" && form.values[name]}>
+                        <TextField
+                            name={name}
+                            id={name}
+                            type={type}
+                            label={label}
+                            disabled={disabled}
+                            error={form.errors[name] && form.touched[name] ?
+                                true : false}
+                            helperText={form.errors[name] && form.touched[name] ?
+                                form.errors[name] : " "}
+                            fullWidth
+                            variant="outlined"
+                            sx={{ mb: 1 }}
+                            {...field}
+                            {...rest}
+                        />
+                    </Tooltip>
                 );
             }}
         </Field>
